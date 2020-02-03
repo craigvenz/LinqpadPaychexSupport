d95d5898ca7e35eb3f05ece774c36b8c0c0f5197
Craig Venz <craig.venz@gmail.com>
2020-02-03 11:44:09 -0800

    Added unit tests

--- LinqpadPaychexSupport/LINQPadStringDataCache.cs
+++ LinqpadPaychexSupport/LINQPadStringDataCache.cs
@@ -6,27 +6,42 @@ using System.Security;
 using Lcp.Paychex.Api;
 using Lcp.Paychex.Api.Interfaces;
 using Newtonsoft.Json;
-using static LINQPad.Util;
 using static LinqpadPaychexSupport.Util;
 
 namespace LINQPadPaychexSupport
 {
     public class LINQPadStringDataCache : IPaychexDataCache
     {
+        private readonly ILINQPadUtil _util;
         const string DefaultPrefix = "paychexData_";
-    
+
         private readonly string _prefix;
         private readonly JsonSerializerSettings _settings;
-    
+
         public bool IgnoreCacheReads { get; set; }
-    
-        public LINQPadStringDataCache(string prefix = DefaultPrefix)
+
+        private class DefaultLINQPadUtil : ILINQPadUtil
         {
+            public string LoadString(string key) 
+                => LINQPad.Util.LoadString(key);
+
+            public void SaveString(string key, string value) 
+                => LINQPad.Util.SaveString(key, value);
+
+            public DateTime GetCacheItemTime(string r) 
+                => File.GetLastWriteTime(Path.Combine(LpDataFolder, r));
+        }
+
+        public LINQPadStringDataCache() : this(DefaultPrefix) { }
+        public LINQPadStringDataCache(string prefix = DefaultPrefix) : this(new DefaultLINQPadUtil(), prefix) { }
+        public LINQPadStringDataCache(ILINQPadUtil util, string prefix = DefaultPrefix)
+        {
+            _util = util;
             _prefix = prefix;
             _settings =
                 new JsonSerializerSettings()
                 {
-                    Formatting = Newtonsoft.Json.Formatting.Indented,
+                    Formatting = Formatting.Indented,
                     MissingMemberHandling = MissingMemberHandling.Error,
                     ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                     NullValueHandling = NullValueHandling.Ignore,
@@ -37,8 +52,8 @@ namespace LINQPadPaychexSupport
                         new TolerantEnumConverter()
                     }
                 };
-
         }
+
         public void Clear()
         {
             foreach (var fi in new DirectoryInfo(LpDataFolder).EnumerateFileSystemInfos($"{_prefix}*"))
@@ -56,16 +71,23 @@ namespace LINQPadPaychexSupport
         {
             if (IgnoreCacheReads) 
                 return default;
-            var str = LoadString(CacheKey(r));
+
+            var str = _util.LoadString(CacheKey(r));
             if (string.IsNullOrEmpty(str))
                 return default;
-            var f = File.GetLastWriteTime(Path.Combine(LpDataFolder, CacheKey(r)));
+
+            var f = _util.GetCacheItemTime(
+                Path.Combine(
+                    LpDataFolder, 
+                    CacheKey(r)
+                )
+            );
             Trace.TraceInformation(
                 $"{nameof(LINQPadStringDataCache)} - cache data is from {HowLongAgo(f)}."
             );
             return JsonConvert.DeserializeObject<T>(str, _settings);
         }
-        public void Set<T>(string r, T value) => SaveString(CacheKey(r), JsonConvert.SerializeObject(value, _settings));
+        public void Set<T>(string r, T value) => _util.SaveString(CacheKey(r), JsonConvert.SerializeObject(value, _settings));
         private string CacheKey(string key) => $"{_prefix}{key}";
     }
 }
