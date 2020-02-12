eec36b31ba157918858c0b8741a0005192e649e4
Craig Venz <cvenz@lcptracker.com>
2020-02-11 16:34:15 -0800

    Added unit tests. Updated TokenCache to use interface. Updated stringcache to have a time to live timespan so prevent me having these issues again with cached data not being current.

---
+++ LinqpadPaychexSupport/DefaultLINQPadUtil.cs
@@ -0,0 +1,20 @@
+﻿// LinqpadPaychexSupport.DefaultLINQPadUtil.cs
+// Craig Venz - 02/11/2020 - 3:07 PM
+
+using System;
+using System.IO;
+using LinqpadPaychexSupport;
+
+namespace LINQPadPaychexSupport {
+    internal class DefaultLINQPadUtil : ILINQPadUtil
+    {
+        public string LoadString(string key)
+            => LINQPad.Util.LoadString(key);
+
+        public void SaveString(string key, string value)
+            => LINQPad.Util.SaveString(key, value);
+
+        public DateTime GetCacheItemTime(string r)
+            => File.GetLastWriteTime(Path.Combine(Util.LpDataFolder, r));
+    }
+}
